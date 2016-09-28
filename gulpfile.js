e8c58db1377637e2111d21f1fcb9f2e0880eac2f
Anton Golovin <golovin.anton.sergeevich@gmail.com>
2016-09-28 13:59:17 +0300

    Start

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+
+var path = {
+    dist: { //where
+        fonts: './dist/fonts',
+        css: './dist/css',
+        js: './dist/js',
+        img: './dist/img',
+        libs: './dist/libs',
+        all: './dist',
+    },
+    src: { //frome
+        fonts: './src/fonts/**/*.*',
+        css: './src/css/**/*.*',
+        js: './src/js/**/*.*',
+        img: './src/img/**/*.*',
+        libs: './src/libs/**/*.*',
+    },
+    bowerJson: './bower.json', //bower
+} 
