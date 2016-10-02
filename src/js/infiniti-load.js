089ce1dc93ea8b3207850c20a81f669425ac95da
Anton Golovin <golovin.anton.sergeevich@gmail.com>
2016-10-02 23:10:45 +0300

    Fix infiniti-load

--- src/js/infiniti-load.js
+++ src/js/infiniti-load.js
@@ -1,3 +1,8 @@
 function infinitiLoad() {
-	$(".content-wrapper > li").clone().appendTo(".content-wrapper");
+	if ($('.catalog-content .new-item').length > 0) {
+		$(".content-wrapper > li").clone().appendTo('.new-item');
+	} else {
+		$(".load-inf-button").before('<ul class="new-item"></ul>');
+		$(".content-wrapper > li").clone().appendTo('.new-item');
+	}
 };
