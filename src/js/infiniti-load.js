8952f1fb9a2039a9c9d8ee83222e09a80db5a070
Anton Golovin <golovin.anton.sergeevich@gmail.com>
2016-10-02 23:17:54 +0300

    Fix new-item (added .row)

--- src/js/infiniti-load.js
+++ src/js/infiniti-load.js
@@ -2,7 +2,7 @@ function infinitiLoad() {
 	if ($('.catalog-content .new-item').length > 0) {
 		$(".content-wrapper > li").clone().appendTo('.new-item');
 	} else {
-		$(".load-inf-button").before('<ul class="new-item"></ul>');
+		$(".load-inf-button").before('<ul class="new-item row"></ul>');
 		$(".content-wrapper > li").clone().appendTo('.new-item');
 	}
 };
