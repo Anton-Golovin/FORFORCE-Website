a736ae0418ad984e62771ce1510fc6ad7c82a7e6
Anton Golovin <golovin.anton.sergeevich@gmail.com>
2016-10-03 22:54:06 +0300

    Fixed min-width & anchor in load-inf-btn & added hover-opacity

--- src/js/infiniti-load.js
+++ src/js/infiniti-load.js
@@ -1,8 +1,9 @@
-function infinitiLoad() {
+function infinitiLoad(e) {
 	if ($('.catalog-content .new-item').length > 0) {
 		$(".content-wrapper > li").clone().appendTo('.new-item');
 	} else {
 		$(".load-inf-button").before('<ul class="new-item row"></ul>');
 		$(".content-wrapper > li").clone().appendTo('.new-item');
-	}
+	};
+	e.preventDefault();
 };
